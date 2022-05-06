7810b659bf6e22e6deaae48fd06c8443cb3fdd7f
Thiago Farias <thiago.farias@nb-tfarias.lan>
2022-05-06 18:32:28 +0100

    implements click action to see card detail and click to see module list and content

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,27 +1,56 @@
 const resolvers = {
-    //resolver key
-    Query: {
-        //default 4 parameters (parent, args, context, info)
+  //resolver key
+  Query: {
+    //default 4 parameters (parent, args, context, info)
 
-        //Returns an array of tracks that will be used to populate the homepage grid of our web client
-        tracksForHome: (_, __, { dataSources }) => {
-            return dataSources.trackAPI.getTracksForHome();
-        },
-
-        //get a single track by id, for the track page
-        track: (_, args, {dataSources}) => {
-            return dataSources.trackAPI.getTrack(args.id);
-        },
+    //Returns an array of tracks that will be used to populate the homepage grid of our web client
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
     },
-    //resolver key
-    Track: {
-        author: ({ authorId }, _, { dataSources }) => {
-            return dataSources.trackAPI.getAuthor(authorId);
-        },
-        modules: ( { id }, _, { dataSources }) => {
-            return dataSources.trackAPI.getTrackModules(id);
-        }
-    }
+
+    //get a single track by id, for the track page
+    track: (_, args, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(args.id);
+    },
+
+    module: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getModule(id);
+    },
+  },
+
+  //resolver key
+  Mutation: {
+    //Increment's a track's numberOfViews property
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views fro track ${id}`,
+          track,
+        };
+      } catch (error) {
+        return {
+          code: error.extensions.response.status,
+          success: false,
+          message: error.extensions.response.body,
+          track: null,
+        };
+      }
+    },
+  },
+
+  //resolver key
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id);
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,17 +1,32 @@
-const { gql } = require('apollo-server');
-
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-
-type Query {
+  type Query {
     "Query to get tracks array for the home page"
-    tracksForHome: [Track!]!,
+    tracksForHome: [Track!]!
     "fetch all specific track, provided a track's ID"
     track(id: ID!): Track
-}
+    "Fetch a specific module, provided a module's ID"
+    module(id: ID!): Module!
+  }
 
-"A track is a group of modules that teaches about a specific topic"
-type Track {
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+  }
+
+  "A track is a group of modules that teaches about a specific topic"
+  type Track {
     id: ID!
     "The track's title"
     title: String!
@@ -29,24 +44,28 @@ type Track {
     numberOfViews: Int
     "The track's complete array of Modules"
     modules: [Module!]!
-}
+  }
 
-type Module {
+  type Module {
     id: ID!
     "The module's title"
     title: String!
     "The module's lenght in minutes"
     length: Int
-}
+    "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
+    content: String
+    "The module's video url, for video-based modules"
+    videoUrl: String
+  }
 
-"Author of a complete Track"
-type Author {
+  "Author of a complete Track"
+  type Author {
     id: ID!
     "author's first and last name"
     name: String!
     "author's profile picture url"
     photo: String
-}
+  }
 `;
 
 module.exports = typeDefs;
